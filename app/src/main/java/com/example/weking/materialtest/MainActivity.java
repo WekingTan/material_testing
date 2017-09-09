c40099bc9ea4f63dec72969a171490658166faa2
wekingtan <393205641@qq.com>
2017-09-09 11:31:04 +0800

    悬浮按钮和加入相关代码注释

--- app/src/main/java/com/example/weking/materialtest/MainActivity.java
+++ app/src/main/java/com/example/weking/materialtest/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.weking.materialtest;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -9,6 +10,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,15 +21,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /**
+         * 实例化 Toolbar 并将其传入；
+         * 实例化 DrawerLayout，并由 Toolbar 实现 Actionbar；
+         * 调出 HomeAsUp 按钮，并改变其默认功能和样式；
+         */
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        NavigationView navView = (NavigationView) findViewById(R.id.nav_view);
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
         }
+
+        /**
+         * 实例化 NavigationView，并默认其初始选项；
+         * 当选中其中一个选项时，则关闭 DrawerLayout 的滑动菜单；
+         */
+        NavigationView navView = (NavigationView) findViewById(R.id.nav_view);
         navView.setCheckedItem(R.id.nav_call);
         navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -36,13 +49,30 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+
+        /**
+         * 实例化悬浮按钮；
+         */
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "FAB clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
+    /**
+     *创建菜单；
+     */
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.toolbar, menu);
         return true;
     }
 
+    /**
+     *判断是否按 HomeAsUp 键，其按钮 id 永远是 R.id.home；
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
